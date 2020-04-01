9e06676226caba00740e6bf1f53a69c44ccbec0b
John Zonneveld <jjczon@gmail.com>
2020-04-01 20:48:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,34 @@
 class Shoe
-   attr_accessor :size, :material, :color,   
+  
    attr_reader :brand
 
     def initialize(brand)
         @brand = brand
     end
 
+    def color=(color)
+        @color = color
+    end
 
+    def color
+        @color
+    end
+
+    def size=(size)
+        @size = size
+    end
+
+    def size
+        @size
+    end
+
+    def material=(material)
+        @material = material
+    end
+
+    def material
+        @material
+    end
 
     def condition=(condition)
         @condition = condition
