b9587e87a459e9bfaec3a496ddbb9d8e565a2b55
John Zonneveld <jjczon@gmail.com>
2020-04-01 20:50:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,18 +1,12 @@
 class Shoe
-  
-   attr_reader :brand
+    attr_accessor :color
+    
+    attr_reader :brand
 
     def initialize(brand)
         @brand = brand
     end
 
-    def color=(color)
-        @color = color
-    end
-
-    def color
-        @color
-    end
 
     def size=(size)
         @size = size
